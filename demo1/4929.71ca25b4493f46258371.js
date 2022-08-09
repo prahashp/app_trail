8e1b1d254e0e15f1c1b5c50a3f9135fa12be6549
prahashp <prahashkalai15@gmail.com>
2022-08-09 08:25:47 +0530

    first commit

---
+++ demo1/4929.71ca25b4493f46258371.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkretail_app_ui=self.webpackChunkretail_app_ui||[]).push([[4929],{4929:(H,M,d)=>{d.d(M,{N6:()=>R});var n=d(7716),l=d(2458);d(9490),d(946);let R=(()=>{class i{}return i.\u0275fac=function(t){return new(t||i)},i.\u0275mod=n.\u0275\u0275defineNgModule({type:i}),i.\u0275inj=n.\u0275\u0275defineInjector({imports:[[l.uc,l.BQ],l.uc,l.BQ]}),i})()}}]);
